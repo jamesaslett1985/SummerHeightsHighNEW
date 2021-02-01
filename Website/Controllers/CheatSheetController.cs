50d6400e4e0f2c7664b5ba1df510af3a2ebd9061
jamesa <james.aslett@directlife.co.uk>
2021-02-01 21:06:33 +0000

    Updates to CheatSheet

--- Website/Controllers/CheatSheetController.cs
+++ Website/Controllers/CheatSheetController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using Website.Models;
 
 namespace Website.Controllers
@@ -9,7 +11,18 @@ namespace Website.Controllers
         {
             //you must new up an instance of the Model in the Controller, or the View won't know anything about the properties in the Model
             var CheatSheetModel = new CheatSheet();
-
+            CheatSheetModel.Countries = new List<Country>()
+            {            
+                new Country("USA", "United States of America"),
+                new Country("GB", "United Kingdom"),
+                new Country("GRY", "Germany"),  
+            };
+            CheatSheetModel.Movies = new List<Movie>()
+            {
+        new Movie() { Id = 1, Title = "The Godfather" },
+        new Movie() { Id = 2, Title = "Forrest Gump" },
+        new Movie() { Id = 3, Title = "Fight Club" },
+            };
             return View(CheatSheetModel);
         }
     }
--- Website/Models/CheatSheet.cs
+++ Website/Models/CheatSheet.cs
@@ -39,6 +39,86 @@ namespace Website.Models
         [MaxLength(25)]
         public string TextAreaLength { get; set; }
 
+        public string Gender { get; set; }
+
+        public List<SelectListItem> Genders { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "1", Text = "Male" },
+            new SelectListItem { Value = "2", Text = "Female" },
+        };
+
+        public List<Country> Countries { get; set; }
+        public string SelectedCountry { get; set; }
+
+        public enum DayOfWeek
+        {
+            Sunday = 0,
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6
+        }
+
+        public string SelectedDay { get; set; }
+
+        public List<Movie> Movies { get; set; }
     }
+
+    public class Country 
+    {
+        public string Abbreviation { get; }
+
+        public string Name { get; }
+
+        public string NameAndAbbreviation => $"{Name} - {Abbreviation}";
+
+        public Country(string abbreviation, string name)
+        {
+            Abbreviation = abbreviation;
+            Name = name;
+        } 
+    }
+
+
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+    }
+
+    public class GetterAndSetterLearning
+    {
+        //field - fields tend to be private/protected (scoped to Class) - with fields you have no control over who can get and set the value
+        public bool MyField;
+
+        //property - has a getter/setter/both. Getter = whether property can be read, Setter = where property value can be changed. Properties allow you to have more control over getting/setting
+
+        //can read and write
+        public bool MyProperty1 { get; set; }
+
+        //can only read
+
+        public bool MyProperty2 { get; }
+
+        private bool _myBackingField;
+
+        //property where we have specified what the getters and setters do
+        public bool MyProperty3 
+        { 
+            get 
+            {
+                return _myBackingField;
+            }
+            set
+            {
+                _myBackingField = value;
+            }
+        }
+    }
+
 }
 
