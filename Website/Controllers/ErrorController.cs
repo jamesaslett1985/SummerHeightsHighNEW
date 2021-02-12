495928d9ba47c02fdca46cec2944b75223f0a81c
jamesa <james.aslett@directlife.co.uk>
2021-02-12 16:47:51 +0000

    Updates

---
+++ Website/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Http(int statusCode)
+        {
+            // Log error, e.g. to a database. You can use the HttpContext.Request object to access important information like the requested URL 
+
+            if (statusCode == 404)
+            {
+                return View("Error404"); //return 404 specific View
+            }
+            return View("ErrorGeneric"); //return generic View
+        }
+    }
+}
--- Website/Startup.cs
+++ Website/Startup.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -36,12 +39,45 @@ namespace Website
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
+
+            //EXAMPLE START
+            //In this block, we can log the error and also serve a response to the user, but the page won't look like the rest of the site
+            //app.UseStatusCodePages(async context =>
+            //{
+            //    context.HttpContext.Response.ContentType = "text/html";
+
+            //    if (context.HttpContext.Response.StatusCode == 404)
+            //    {
+            //        // Log this error here, e.g. to a database. You can use the context.HttpContext.Request 
+            //        // object to access important information like the requested URL
+            //    }
+
+            //    await context.HttpContext.Response.WriteAsync(
+            //        "We're <b>really</b> sorry, but something went wrong. Error code: " +
+            //        context.HttpContext.Response.StatusCode);
+            //});
+            //EXAMPLE END
+
+            //This is a better alternative to the above. It will respond with a redirect instead of an actual message. Here, all HTTP status errors will be redirected to a method called Http on the ErrorController
+            app.UseStatusCodePagesWithRedirects("/Error/Http?statusCode={0}");
+
+            //allows static files in wwwroot to be served
             app.UseStaticFiles();
+            //allow visitors to browse the wwwroot/img folder by going to https://localhost:44345/img/
+            app.UseDirectoryBrowser(new DirectoryBrowserOptions
+            {
+                FileProvider = new PhysicalFileProvider
+   (
+       Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img")
+   ),
+                RequestPath = "/img"
+            });
 
             app.UseRouting();
 
